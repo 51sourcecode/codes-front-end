b3cd93025ed23654ad8cc3203176feb0dc700dae
Richard.He <cn.richard.he@gmail.com>
2016-12-29 18:20:40 +0800

    Rename

--- phoenix-ui/resources/js/common/loadHtml.js
+++
@@ -1,7 +0,0 @@
-define(function(require, exports, module){
-	var $ = require('jquery');
-	//动态加载首尾页面
-	$('#header').load('/htmlv4/common/header.html');
-	$('#footer').load('/htmlv4/common/footer.html');	
-	
-});
